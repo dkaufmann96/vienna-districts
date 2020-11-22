2b03f8b10c093d85408964992d8c08da7a82b4ae
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2020-11-22 11:39:51 +0100

    [FEATURE] add quiz-mode component that extends map

---
+++ src/components/QuizMap.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="flex flex-row">
+      <span class="text-gray-700" v-if="randomDistrict"
+        >Wählen Sie den
+        <b>{{ randomDistrict.feature.properties.BEZ }}. Bezirk</b> aus!</span
+      >
+      <span class="text-green-500" v-if="correctlyChosen"
+        >Richtige Auswahl</span
+      >
+      <span class="text-red-500" v-if="correctlyChosen === false"
+        >Falsche Auswahl: Der
+        <b>{{ chosenDistrict.feature.properties.BEZ }}. Bezirk</b> wurde
+        ausgewählt.</span
+      >
+    </div>
+    <div id="map" class="bg-white min-h-screen" />
+  </div>
+</template>
+
+<script>
+import Map from "./Map.vue";
+
+export default {
+  name: "QuizMap",
+  extends: Map,
+  data() {
+    return {
+      randomDistrict: undefined,
+      chosenDistrict: undefined,
+    };
+  },
+  methods: {
+    chooseRandomDistrict() {
+      const randomIndex = Math.floor(
+        Math.random() * Math.floor(this.layers.length)
+      );
+      this.randomDistrict = this.layers[randomIndex];
+    },
+    startQuiz() {
+      this.chooseRandomDistrict();
+      this.layers.forEach((layer) => {
+        this.resetLayer(layer);
+        layer.unbindTooltip();
+        layer.on("click", (event) => {
+          this.chosenDistrict = event.target;
+          this.chosenDistrict.removeEventListener("mouseover");
+          this.chosenDistrict.removeEventListener("mouseout");
+          if (this.correctlyChosen) {
+            this.chosenDistrict.setStyle({
+              fillColor: "green",
+            });
+            setTimeout(() => {
+              this.chosenDistrict = null;
+              this.startQuiz();
+            }, 1500);
+          } else {
+            event.target.setStyle({
+              fillColor: "red",
+            });
+          }
+        });
+      });
+    },
+  },
+  computed: {
+    correctlyChosen() {
+      if (!this.chosenDistrict || !this.randomDistrict) {
+        return undefined;
+      }
+      return (
+        this.randomDistrict.feature.properties.BEZNR ===
+        this.chosenDistrict.feature.properties.BEZNR
+      );
+    },
+  },
+  async mounted() {
+    this.startQuiz();
+  },
+};
+</script>
