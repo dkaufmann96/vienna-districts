5c3e3484478381256e76956c0882d7268da434e1
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2021-02-07 13:31:57 +0100

    [FEATURE] add dedicated map menu

---
+++ src/components/menu/MapMenu.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="absolute right-0 md:right-0 top-36 md:top-10 z-top">
+    <QuizButton @click="toggleQuizMode" class="mb-2" />
+  </div>
+</template>
+
+<script>
+import QuizButton from "../quiz/QuizButton.vue";
+
+export default {
+  name: "MapOverlay",
+  emits: ["quiz-mode-toggled"],
+  components: {
+    QuizButton,
+  },
+  props: {
+    quizMode: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  inject: ["global"],
+  methods: {
+    toggleQuizMode() {
+      this.$emit("quiz-mode-toggled");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.map-overlay:hover .menu {
+  display: block;
+}
+.menu {
+  display: none;
+  background-color: transparent;
+}
+</style>
