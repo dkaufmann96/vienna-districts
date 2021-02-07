a8d02eb5e81d746c08f69e39ea6a4b29c788d441
Daniel Kaufmann <daniel.kaufmann96@gmx.at>
2021-02-07 13:30:12 +0100

    [FEATURE] restyle quiz button

---
+++ src/components/quiz/QuizButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <a
+    @click="global.toggleQuizMode()"
+    class="quiz-button rounded-l-md bg-black p-3 text-md md:text-lg cursor-pointer text-center text-white bg-opacity-50 text-opacity-100"
+  >
+    Quiz
+  </a>
+</template>
+
+<script>
+export default {
+  name: "QuizButton",
+  inject: ["global"],
+};
+</script>
+
+<style scoped>
+.quiz-button {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
